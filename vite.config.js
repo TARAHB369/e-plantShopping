cfb84ce6a005bcb9d7b5a2874699aa7d1450ef59
TARAHB369 <sudeeep369@gmail.com>
2024-07-23 20:20:08 +0545

    hey

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base:"/e-plantShopping-github/",
-  base: "/shoppingreact",
   plugins: [react()],
+  base: '/e-plantShopping/',  // replace 'your-repo-name' with your actual repository name
 })
